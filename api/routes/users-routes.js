fef718f0a2fa29e02f700eb4998caaa23af732be
Santiago <santiango.zanetti@cardenalspionla.com.ar>
2023-09-06 14:55:12 -0300

    ruta_logout terminada

--- api/routes/users-routes.js
+++ api/routes/users-routes.js
@@ -1,32 +1,36 @@
-const express = require('express')
-const router = express.Router()
-const User = require('../models/Users')
-const { generateToken } = require('../config/tokens')
+const express = require("express");
+const router = express.Router();
+const User = require("../models/Users");
+const { generateToken } = require("../config/tokens");
 
+router.post("/login", (req, res, next) => {
+  const { email, password } = req.body;
+  User.findOne({
+    where: {
+      email,
+    },
+  }).then((user) => {
+    if (!user) return res.send(401).json({ message: "User no encontrado" });
 
+    user.validatePassword(password).then((isValid) => {
+      if (!isValid)
+        return res.send(401).json({ message: "Contraseña no validada" });
+      else {
+        const payload = {
+          email: user.email,
+          name: user.name,
+          last_name: user.last_name,
+        };
+        const token = generateToken(payload);
+        res.cookie("token", token);
+        res.send(payload);
+      }
+    });
+  });
+});
 
-router.post('/login', (req, res, next) => {
-    const { email, password } = req.body
-    User.findOne({
-        where: {
-            email,
-        },
-    }).then((user) => {
-        if (!user) return res.send(401).json({message: 'User no encontrado'})
+router.post("/logout", (req, res) => {
+  res.clearCookie("token"); // borro la cookie de token
 
-        user.validatePassword(password).then((isValid) => {
-            if (!isValid) return res.send(401).json({message: 'Contraseña no validada'})
-            else {
-                const payload = {
-                    email: user.email,
-                    name: user.name,
-                    last_name: user.last_name
-                };
-                const token = generateToken(payload)
-                res.cookie('token', token)
-                res.send(payload)
-            }
-        })
-    })
-})
-
+  res.sendStatus(204);
+});
