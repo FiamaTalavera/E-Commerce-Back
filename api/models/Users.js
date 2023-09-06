8d39ce4a04f86126101e288e2f648eb520b007ba
Isidro-Molina <isidromolina260@gmail.com>
2023-09-06 13:04:24 -0300

    creacion ruta para login

--- api/models/Users.js
+++ api/models/Users.js
@@ -1,4 +1,4 @@
-const { DataTypes, Model } = require("sequielize");
+const { DataTypes, Model } = require("sequelize");
 const bcrypt = require("bcrypt");
 const sequelize = require("../config/db");
 
