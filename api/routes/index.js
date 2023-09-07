6e53e5edf2a25bdd41115cc04a82412e286ffb26
FiamaTalavera <fiama.viccini@gmail.com>
2023-09-07 16:36:17 -0300

    Se crea la ruta para aliminar un producto del carrito

--- api/routes/index.js
+++ api/routes/index.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const products = require("./products-routes")
 const user = require("./users-routes")
+const orders = require("./orders-routes")
 
 router.use("/products", products)
 router.use("/user", user)
+router.use("/order", orders)
 
 module.exports = router;
