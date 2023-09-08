a628fa3ff400f008ca9a17c4affcb8ebca5567db
Isidro-Molina <isidromolina260@gmail.com>
2023-09-08 16:07:56 -0300

    ruta para ver los productos del chango

--- api/routes/index.js
+++ api/routes/index.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const products = require("./products-routes")
 const user = require("./users-routes")
+const order = require("./orders-routes")
 
 router.use("/products", products)
 router.use("/user", user)
+router.use("/order", order)
 
 module.exports = router;
---
+++ api/routes/orders-routes.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const router = express.Router();
+const Order = require('../models/Orders');
+const { validateUser } = require('../middlewares/auth');
+const Product = require('../models/Products');
+
+router.get('/', validateUser, (req, res, next) => {
+    // para sacar las ordenes del usuario logueado
+    const { userId } = req.user;
+    Order.findAll({
+        where: {
+            userId,
+        },
+    })
+        .then((orders) => {
+            // array de promesas
+            const promises = orders.map((order => {
+                return Product.findOne({
+                    where: {
+                        id: order.productId
+                    },
+                })
+                    .then((product) => {
+                        return {
+                            // convertimos lo de Sequelize a un objeto JS
+                            ...order.toJSON(),
+                            product
+                        };
+                    });
+            }));
+            return Promise.all(promises)
+        })
+        .then((ordersWithData) => {
+            console.log(ordersWithData);
+            res.status(200).send(ordersWithData)
+        })
+        .catch(next);
+});
+
+module.exports = router;
